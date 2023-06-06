ed39c21277c361fb5ecf0656a18d96d6e88c8176
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-06-06 16:55:51 +0530

    first commit

---
+++ week2-task2/set practice.py
@@ -0,0 +1,189 @@
+
+##set built functions
+##set{}
+##set wont allow te duplicate
+##add()
+
+##thisset = {"apple", "banana", "cherry"}
+##
+##thisset.add(1,"orange")
+##
+##print(thisset)
+##
+##
+##var={"names","flowers","fruits"}
+##var.add("games")
+##print(var)
+##
+##
+##a={1,2,1,2,2,3,1,1,1,}
+##a.add(3)
+##print(a)
+
+##a={31782556824715,47373473,5357347}
+##a.add(666666)
+##print(a)
+
+##clear()---------------------------------------
+##s={"apple", "banana", "cherry"}
+##s.clear()
+##print(s)
+
+##a={"eqiwiwrlluwglIFUWRVIRE84396464"}
+##a.clear()
+##print(a)
+##
+
+
+##copy()----------------------------------------
+##fruits = {"apple", "banana", "cherry"}
+##x = fruits.copy()
+##print(x)
+
+##a={"dliaeu5a67 ytlaerl4glsadvbw4ty2iyt"}
+##b=a.copy()
+##print(b)
+##
+
+##names={"gayathri","kranth","kumar"}
+##a=names.copy()
+##print(a)
+##
+##c={"chocolates"}
+##d=c.copy()
+##print(d)
+
+##difference update()---------------------------
+
+##x = {"apple", "banana", "cherry"}
+##y = {"google", "microsoft","apple"}
+##y.difference_update(x)
+##
+##print(y)
+##
+##
+##a={"roses","lilly","apple"}
+##b={"apple","vivo","redmi"}
+##a.difference_update(b)
+##print(a)
+
+##discard()----------------------------------
+##a= {"father", "mother", "me"}
+##a.discard("me")
+##print(a)
+##
+##x={"gayathri","sravanthi","happy"}
+##y={"hi","hello","namaste"}
+##x.discard("sravanthi")
+##print(x)
+##
+##y={"hi","hello","namaste"}
+##y.discard("namaste")
+##print(y)
+##
+##x={"hi hellonamaste"}
+##x.discard("hi hellonamaste")
+##print(x)
+##
+##x={1,33,721,48793,6717,1471,71}
+##x.discard(1)
+##print(x)
+
+##intersection()------------------------
+##y={"hi","hello","namaste"}
+##z={"hi","im","gaya",3}
+##x=z.intersection(y)
+##x1=y.intersection(z)
+##print(x)
+##print(x1)
+##intersection update()------------------
+##x = {"apple", "banana", "cherry"}
+##y = {"google", "microsoft", "apple"}
+##x.intersection_update(y) 
+##print(x)
+##
+##
+##a={"sad","happy","angry"}
+##b={"happy","gaps"}
+##a.intersection_update(b)
+##print(a)
+
+
+##issubset()-------------------
+##a={"gayathri","jhansi","divya"}
+##b={"friends","devils","ghosts"}
+##print(a.issubset(b))
+##
+
+##issuperset()-------------------
+##a={"gayathri","jhansi","divya","sailaja"}
+##b={"friends","devils","ghosts","gayathri"}
+##print(a.issuperset(b))
+##print(b.issuperset(a))
+
+##isdisjoint()--------------------
+##a={"gayathri","jhansi","divya"}
+##b={"friends","devils","ghosts"}
+##print(a.isdisjoint(b))
+
+
+##symmetric difference()----------
+
+##a={"krupa","kavi","gai","lav"}
+##b={"sis","friends","devils"}
+##c=a.symmetric_difference(b)
+##print(c)
+##
+
+##symmetric difference update()----------------
+##a={"krupa","kavi","gai","lav"}
+##b={"sis","friends","devils"}
+##a.symmetric_difference_update(b)
+##print(a)
+##
+##
+##union()----------------
+
+##x = {"apple", "banana", "cherry"}
+##y = {"google", "microsoft", "apple"}
+##z = x.union(y) 
+##print(z)
+
+
+##update()-----------------------
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.update(y) 
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
