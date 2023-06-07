5a85bc948bd39863427d3f8ce84880d87c034bc2
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-06-07 18:01:15 +0530

    first commit

---
+++ week2-task3/dict practice.py
@@ -0,0 +1,95 @@
+##dict methods------------------
+##keys and values
+##clear()---removes all elemmnts from dict
+##car ={ "brand": "Ford","model": "Mustang","year": 1964}
+##car.clear()
+##print(car)
+
+
+
+##a={"name":"gayathri","age":20}
+##print(a)
+
+##a={{"company"}:"sightspectrum",{"year"}:2017}
+##print(a)
+
+##a={"hi":{"hello":("hi",675)}}
+##print(a)
+##keys only--------
+##a={"hi":1,"hello":2,"namaste":3}
+##b=a.keys()
+##print(b)
+##
+##a={"gai":1,"div":2,"jhanu":3,"sailu":4}
+##b=a.keys()
+##print(b)
+
+
+
+##values only--------
+##a={"hi":"ekfwag","efuiwe":"r4itfai"}
+##b=a.values()
+##print(b)
+##from keys()---------
+##x = ('key1', 'key2', 'key3')
+##y = 0
+##thisdict = dict.fromkeys(x, y)
+##print(thisdict)
+##
+##
+##get()------------
+##car = {"brand": "Ford","model": "Mustang","year": 1964}
+##x = car.get("model")
+##print(x)
+##car = {"brand": "Ford","model": "Mustang","year": 1964}
+##x = car.get("brand")
+##print(x)
+
+##
+##pop()-------------------------
+##car = {"brand": "Ford","model": "Mustang","year": 1964}
+##car.pop("model")
+##print(car)
+##car = {"brand": "Ford","model": "Mustang","year": 1964}
+##car.pop("brand")
+##print(car)
+##
+##
+##
+##car = { "brand": "Ford", "model": "Mustang","year": 1964}
+##car.pop("model")
+##print(car)
+##
+##
+##
+##
+##itemss()----------------
+car = {"brand": "Ford","model": "Mustang","year": 1964}
+x = car.items()
+print(x)
+
+car = {"brand": "Ford","model": "Mustang","year": 1964}
+x = car.items()
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
