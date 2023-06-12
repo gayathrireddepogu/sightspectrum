0fde9cd6e9267f4e5163e3876b40e5445ef2fe91
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-06-12 17:26:23 +0530

    first commit

---
+++ week3-task1/if else practice.py
@@ -0,0 +1,198 @@
+##if else statement()
+##a==b:
+##a = 33
+##b = 33
+##
+##if b == a:
+##  print("b is equal to a")
+##
+##
+##a=54
+##b=54
+##if a==b:
+##   print("a is  equal to b")
+
+##not equal  !=
+
+##a=54
+##b=45
+##if a!=b:
+##   print("a is not equal to b")
+
+##a=54
+##b=54
+##c=44
+##d=56
+##if a==b!=c<=d:
+##   print("a is  equal to b not equal to c is greater than d")
+
+##   
+##
+##nested if------------
+##if x<=40:
+##    if x==44:
+##        print("gayu")
+##elif x!=42:
+##    print("sailu")
+##else:
+##    print("jhanu")
+##print("divya")
+##print("dhfkjagh")
+##print("efhrrg")
+##    
+##a=34
+##b=54
+##if a<=b:
+##    print("b is not equal to a")
+##for loop()----------------------
+##for x in range(2,20):
+##    print(x)
+##
+##    
+##fruits = ["apple", "banana", "cherry"]
+##for x in fruits:
+##  print(x) 
+##
+##
+##a=["gayathri","gayathri","gwkergye"]
+##for x in a:
+##    print(x)
+##a=(1,9)
+##for x in range(2,20,4):
+##    print(x)
+##    x+=1
+##
+##
+##fruits = ["apple", "banana", "cherry"]
+##for a in fruits:
+##  print(x) 
+##  if a == "cherry":
+##      continue
+##    
+##    
+##for g in "gayathri":
+##     print(g)
+## 
+##
+##for g in "sightspectrum":
+##    print(g)
+##
+##while loop ----------------
+##a=8
+##while a!=40:
+##    print(a)
+##    a+=1
+
+##amount=200
+##discount=0.1*amount if amount<200 else 180
+##print(discount)
+
+
+
+##names=["gayathri","jhansi","sailu","divya"]
+##for x in names:
+##    print(x)
+##
+##    if x=="divya":
+##       break
+##
+##
+##
+##names=["gayathri","jhansi","sailu","divya"]
+##for x in names:
+##    print(x)
+##
+##    if x=="sailu":
+##        
+##        continue
+##       
+##
+##
+##ranges of for loop---------------
+##
+##for a in range(9):
+##    print(a)
+##for a in range(2,12,4):
+##    print(a)
+##for a in range(22,44,66):
+##    print(a)
+##
+####else statement in for loop---------------
+
+##for a in range(9):
+##    print(a)
+##else:
+##    print("it's
+##          completd")
+##
+
+##nested loop---------------
+
+names = ["sweet", "happy", "nice"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in names:
+  for y in fruits:
+    print(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
