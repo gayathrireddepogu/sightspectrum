61ae97da87112cdfabb1d8e1c03d266287b9c555
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-06-14 15:39:45 +0530

    first commit

---
+++ week3-task3/if else elif.py
@@ -0,0 +1,186 @@
+if
+elif
+else  statements-----------------
+age = 18
+if age>=18:
+    print("helo")
+
+age = 18
+if age==18:
+    print("helo your welcome")
+
+
+age = 18
+if age !=18:
+    print("helo")
+
+
+a = 33
+b = 200
+
+if b > a:
+print("b is greater than a")
+ nested if-----------------
+a = 33
+b = 200
+
+if b > a:
+    print("b is greater than a")
+
+
+
+var="gayathri is good girl"
+var1="she is good girl"
+if var!=var1:
+    print("var is  not equal to var1")
+
+else:
+    print("hi")
+name= input("enter your name:")
+age = int(input("enter your age:"))
+if age>=18:
+    print("hi you are welcome")
+else:
+    print("sry")
+marks = float(input("enter your marks:"))
+if marks>=90:
+              
+              print("your grade is : A")
+elif marks >=80 and marks <90:
+    print("your grade is :B")
+elif marks >=70 and marks <80:
+    print("your grade is :c")
+elif marks >=70 and marks <70:
+    print("print grade is :D")
+else:
+    print("sorry you are fail")
+    
+    
+name =input("enter your name :")
+age = int(input("enter your age:"))
+study=input("enter your qualification:")
+
+if age >=18:
+    if study =="btech":
+        
+        print ("great degree btech","name","hello")
+elif  study <="btech":
+    
+        print ("great degree ","name","hello")
+else:
+    print("your are selected")
+        
+              
+##name =input("enter your name :")
+age = int(input("enter your age:"))
+study=input("enter your qualification:")
+
+if age >=18:
+    if study =="btech":
+         print ("great degree btech",name,"hello")
+    else:
+ 
+    
+        print ("great degree ",name,"hello")
+else:
+##    print("your are selected")
+short hand if statement-----
+a=2
+b=2
+
+if a ==b: print("a is equal to b")
+
+a=4546
+b=36365
+if a!=b: print(" a is not equal to b")
+
+
+a="gayathri"
+b="jhansi"
+if a!=b: print("a is not equal to b")
+
+a=352
+b=34
+if a>b: print("a is greater than b")
+
+
+short hand if else statement---
+##
+a=45
+b=53
+print("A") if a>b  else print("B")
+
+a="gayathri"
+b="jhansi"
+print("A") if a!=b else print("b")
+
+
+a="gayathri"
+b="jhansi"
+print("A") if b!=a else print("B")
+
+a="gayathri"
+b="jhansi"
+c="sailu"
+if b!=a and b!=c:
+    print("both condition are not equal")
+
+
+a="fruits"
+b="flowrs"
+c="food"
+if a!=c and b!=a:
+    print("both conditions are false")
+
+
+a="gayathri"
+b="jhansi"
+c="sailu"
+if b!=a or b!=c:
+    print("both condition are not equal")
+
+
+a="fruits"
+b="flowrs"
+c="food"
+if a!=c or b!=a:
+    print("both conditions are false")
+
+a="gayathri"
+b="jhansi"
+c="sailu"
+if b!=a or a!=c:
+    
+    print("a is NOT equal to b and c")
+
+
+a=25
+b=44
+c=64
+if a<c and b!=a:
+    print("a is NOT less than c and b is NOT equal to a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
