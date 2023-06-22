9c4fec24409f50fb482bc7ee6e0b077fbc6e1df6
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-06-22 17:01:50 +0530

    first commit

---
+++ week4-task4/task-4.py
@@ -0,0 +1,145 @@
+def fun(**name):
+    return name
+print(name)
+##    print(name)
+fun(name="gayahthri",age=20)
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+gen = my_generator()
+
+print(next(gen))  
+print(next(gen))  
+print(next(gen))
+
+x = "hello"
+
+#if condition returns False, AssertionError is raised:
+assert x == "goodbye", "x should be 'hello'"
+x = "hello"
+def fun(x)
+assert x == "hello":
+    print(x)
+fun(x)
+
+
+assert x == "goodbye"
+def fun(name):
+    print(name)
+##fun("gayathri")
+pass
+def outer_function():
+    num=23
+    def inner_function():
+        num=22
+        print("This is a local function.")
+        inner_function()
+        print("this is a non local function")
+## def inner_function():
+##
+##outer_function()
+print("this is local and global")
+
+yield-------
+def fun():
+    a=5
+    b=6
+    c=a*b
+    yield c
+    yield a
+    yield b
+var=fun()
+print(next(var))
+print(next(var))
+print(next(var))
+
+
+yield-------
+def fun():
+    a="gayu"
+    b="janu"
+    c=len(a)*len(b)
+    yield c
+    yield a
+    yield b
+var=fun()
+print(next(var))
+print(next(var))
+print(next(var))
+
+def fun(name):
+    print(name+"gayu")
+fun("krish")
+fun("gopi")
+
+def fun(name4,name3,name2):
+    print(name4 + "gayu","frnd")
+fun("krish","gs",7)
+fun("gopi",7,8)
+fun("janu",7,9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
